b72e32225907e0da47e0b6f02061f68e5154a9b3
hiro <hiroshima6504@gmail.com>
2017-10-17 22:07:08 +0000

    wont_9_3

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,4 +1,4 @@
-@if ($items)
+@@if ($items)
     <div class="row">
         @foreach ($items as $item)
             <div class="item">
@@ -9,6 +9,9 @@
                         </div>
                         <div class="panel-body">
                             <p class="item-title"><a href="#">{{ $item->name }}</a></p>
+                            <div class="buttons text-center">
+                                @include('items.want_button', ['item' => $item])
+                            </div>
                         </div>
                     </div>
                 </div>
