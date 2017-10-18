d8b347381f172483f18179adbfad2898367d01cb
hiro <hiroshima6504@gmail.com>
2017-10-18 03:06:54 +0000

    want_have

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -17,7 +17,11 @@
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
                                 @endif
+                                 @if (Auth::check())
+                                    @include('items.have_button', ['item' => $item])
+                                @endif
                             </div>
+                            
                         </div>
                     </div>
                 </div>
